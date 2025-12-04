9eb833879eb77f1ebebd2735c8cde5e6e715e5ce
Deepa131 <deepapaudel65@gmail.com>
2025-12-04 16:43:43 +0545

    splash screen done

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,10 +1,48 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    //Auto navigate after 2 seconds
+    Timer(const Duration(seconds: 2), () {
+      Navigator.pushReplacementNamed(context, '/onboarding');
+    });
+  }
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 179, 200, 210),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 150,
+              width: 150,
+              child: Image.asset('assets/images/image.jpg', fit: BoxFit.contain,
+              ),
+            ),
+            const SizedBox(height: 20),
+
+            const Text("RentEasy",
+            style: TextStyle(
+              fontSize: 28,
+              fontWeight: FontWeight.bold,
+              color: Colors.black87,
+            ),)
+          ],
+        ),
+      ),
+    );
   }
 }
