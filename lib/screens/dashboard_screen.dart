1426fb612368c22ea2c20c5284263c045f91bc6b
Deepa131 <deepapaudel65@gmail.com>
2025-12-11 22:34:02 +0545

    error solved

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -22,7 +22,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Bottom Navigation'),
+        title: const Text('Admin Dashboard'),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
       body: lstBottomScreen[_selectedIndex],
