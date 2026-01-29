78e83a91786dd299fe19b5b46ba12ba1f5154652
Deepa131 <deepapaudel65@gmail.com>
2026-01-29 18:28:16 +0545

    updated hive_service

--- lib/core/services/hive/hive_service.dart
+++ lib/core/services/hive/hive_service.dart
@@ -2,7 +2,9 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:room_rental/core/constants/auth_hive_constants.dart';
+import 'package:room_rental/features/add_room/data/models/add_room_hive_model.dart';
 import 'package:room_rental/features/auth/data/models/auth_hive_model.dart';
+import 'package:room_rental/features/room_type/data/models/room_type_hive_model.dart';
 
 final hiveServiceProvider = Provider<HiveService>((ref) {
   return HiveService();
@@ -16,6 +18,26 @@ class HiveService {
     Hive.init(path);
     _registerAdapters();
     await openBoxes();
+    await insertRoomTypeDummyData();
+  }
+
+  Future<void> insertRoomTypeDummyData() async {
+    final typeBox = Hive.box<RoomTypeHiveModel>(AuthHiveConstants.roomTypeTable);
+
+    if (typeBox.isNotEmpty) {
+      return;
+    }
+
+    final dummyRoomTypes = [
+      RoomTypeHiveModel(typeName: 'Single room'),
+      RoomTypeHiveModel(typeName: '1 BHK'),
+      RoomTypeHiveModel(typeName: '2 BHK'),
+      RoomTypeHiveModel(typeName: 'Studio room'),
+    ];
+
+    for (var type in dummyRoomTypes) {
+      await typeBox.put(type.typeId, type);
+    }
   }
 
   //register adapters
@@ -23,11 +45,19 @@ class HiveService {
     if (!Hive.isAdapterRegistered(AuthHiveConstants.authTypeId)) {
       Hive.registerAdapter(AuthHiveModelAdapter());
     }
+    if (!Hive.isAdapterRegistered(AuthHiveConstants.roomTypeId)) {
+      Hive.registerAdapter(RoomTypeHiveModelAdapter());
+    }
+    if (!Hive.isAdapterRegistered(AuthHiveConstants.addRoomTypeId)) {
+      Hive.registerAdapter(AddRoomHiveModelAdapter());
+    }
   }
 
   // open boxes
   Future<void> openBoxes() async {
     await Hive.openBox<AuthHiveModel>(AuthHiveConstants.authTable);
+    await Hive.openBox<RoomTypeHiveModel>(AuthHiveConstants.roomTypeTable);
+    await Hive.openBox<AddRoomHiveModel>(AuthHiveConstants.addRoomTable);
   }
 
   //close hive
@@ -68,5 +98,67 @@ class HiveService {
   bool isEmailRegistered(String email) {
     final users = _authBox.values.where((user) => user.email == email);
     return users.isNotEmpty;
+  }
+
+  // room type box
+  Box<RoomTypeHiveModel> get _typeBox =>
+      Hive.box<RoomTypeHiveModel>(AuthHiveConstants.roomTypeTable);
+
+  Future<RoomTypeHiveModel> createType(RoomTypeHiveModel type) async {
+    await _typeBox.put(type.typeId, type);
+    return type;
+  }
+
+  List<RoomTypeHiveModel> getAllTypes() {
+    return _typeBox.values.toList();
+  }
+
+  RoomTypeHiveModel? getTypeById(String typeId) {
+    return _typeBox.get(typeId);
+  }
+
+  Future<bool> updateType(RoomTypeHiveModel type) async {
+    if (_typeBox.containsKey(type.typeId)) {
+      await _typeBox.put(type.typeId, type);
+      return true;
+    }
+    return false;
+  }
+
+  Future<void> deleteType(String typeId) async {
+    await _typeBox.delete(typeId);
+  }
+
+  // add room box
+  Box<AddRoomHiveModel> get _roomBox =>
+  Hive.box<AddRoomHiveModel>(AuthHiveConstants.addRoomTable);
+
+  Future<AddRoomHiveModel> createRoom(AddRoomHiveModel room) async {
+    await _roomBox.put(room.roomId, room);
+    return room;
+  }
+
+  List<AddRoomHiveModel> getAllRooms() {
+    return _roomBox.values.toList();
+  }
+
+  AddRoomHiveModel? getRoomById(String roomId) {
+    return _roomBox.get(roomId);
+  }
+
+  Future<List<AddRoomHiveModel>> getRoomsByOwner(String ownerId) async {
+    return _roomBox.values.where((room) => room.ownerId == ownerId).toList();
+  }
+
+  Future<bool> updateRoom(AddRoomHiveModel room) async {
+    if (_roomBox.containsKey(room.roomId)) {
+      await _roomBox.put(room.roomId, room);
+      return true;
+    }
+    return false;
+  }
+
+  Future<void> deleteRoom(String roomId) async {
+    await _roomBox.delete(roomId);
   }
 }
