8d5b7fc6edabf0cf95b5216b4f026a95649cf530
Deepa131 <deepapaudel65@gmail.com>
2026-01-29 18:26:29 +0545

    updated api_endpoints

--- lib/core/api/api_endpoints.dart
+++ lib/core/api/api_endpoints.dart
@@ -5,21 +5,21 @@ import 'package:flutter/foundation.dart';
 class ApiEndpoints {
   ApiEndpoints._();
 
-  static const bool isPhysicalDevice = false;
+  static const bool isPhysicalDevice = false;  // make port 5050
 
   static const String compIpAddress = "192.168.1.1";
 
   static String get baseUrl {
     if (isPhysicalDevice) {
-      return 'http://$compIpAddress:3000/api/v1';
+      return 'http://$compIpAddress:5050/api/';
     }
     //yedi android
     if (kIsWeb) {
-      return 'http://localhost:3000/api/v1';
+      return 'http://localhost:5050/api';
     } else if (Platform.isAndroid) {
-      return 'http://10.0.2.2:3000/api/v1';
+      return 'http://10.0.2.2:5050/api';
     } else {
-      return 'http://10.0.2.2:3000/api/v1';
+      return 'http://10.0.2.2:5050/api';
     }
   }
 
@@ -31,4 +31,13 @@ class ApiEndpoints {
   static const String userRegister = '/users/register';
   static String userById(String id) => '/users/$id';
   static String userPhoto(String id) => 'users/$id/photo';
+
+  static const String types = '/roomTypes';
+  static String typeById(String id) => '/roomTypes/$id';
+
+  static const String rooms = '/rooms';
+  static String roomById(String id) => '/rooms/$id';
+  static const String uploadRoomImage = '/rooms/upload-image';
+  static const String uploadRoomVideo = '/rooms/upload-video';
+
 }
