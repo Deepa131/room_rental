7e68b1ddf6668e0998de0be5abefaee09a186432
Deepa131 <deepapaudel65@gmail.com>
2026-02-23 02:00:40 +0545

    updated add room entity for adding map

--- lib/features/add_room/domain/entities/add_room_entity.dart
+++ lib/features/add_room/domain/entities/add_room_entity.dart
@@ -1,14 +1,31 @@
 import 'package:equatable/equatable.dart';
 import 'package:room_rental/features/room_type/domain/entities/room_type_entity.dart';
 
+class LocationCoordsEntity extends Equatable {
+  final double latitude;
+  final double longitude;
+  final String? address;
+
+  const LocationCoordsEntity({
+    required this.latitude,
+    required this.longitude,
+    this.address,
+  });
+
+  @override
+  List<Object?> get props => [latitude, longitude, address];
+}
+
 class AddRoomEntity extends Equatable {
   final String? roomId;
   final String? ownerId;
+  final String? ownerName;
   final String ownerContactNumber;
 
   final String roomTitle;
   final double monthlyPrice;
   final String location;
+  final LocationCoordsEntity? locationCoords;
   final RoomTypeEntity roomType;
   final String? description;
 
@@ -22,10 +39,12 @@ class AddRoomEntity extends Equatable {
   const AddRoomEntity({
     this.roomId,
     this.ownerId,
+    this.ownerName,
     required this.ownerContactNumber,
     required this.roomTitle,
     required this.monthlyPrice,
     required this.location,
+    this.locationCoords,
     required this.roomType,
     this.description,
     this.images,
@@ -39,10 +58,12 @@ class AddRoomEntity extends Equatable {
   List<Object?> get props => [
         roomId,
         ownerId,
+        ownerName,
         ownerContactNumber,
         roomTitle,
         monthlyPrice,
         location,
+        locationCoords,
         roomType,
         description,
         images,
